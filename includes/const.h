1a091f222e270ad5e7a8c42995983726e92ed2db
whatswitheffy <48794170+whatswitheffy@users.noreply.github.com>
2021-04-21 03:07:18 +0600

    Add files via upload

---
+++ includes/const.h
@@ -0,0 +1,28 @@
+#ifndef CONST_H
+#define CONST_H
+#include <string>
+using namespace std;
+enum OPERATOR {
+    LSQRBRACKET, RSQRBRACKET, SIZE,
+    IF, THEN,
+    ELSE, ENDIF,
+    WHILE, ENDWHILE,
+    GOTO, COLON,
+    LBRACKET, RBRACKET,
+    ASSIGN,
+    OR,
+    AND,
+    BITOR,
+    XOR,
+    BITAND,
+    EQ, NEQ,
+    LEQ, LT,
+    GEQ, GT,
+    SHL, SHR,
+    PLUS, MINUS,
+    MULTIPLY, DIV, MOD
+};
+extern string OPERTEXT[];
+extern int PRIORITY[];
+extern int OPER_LEN;
+#endif
---
+++ includes/syntax.h
@@ -0,0 +1,7 @@
+#ifndef SYNTAX_H
+#define SYNTAX_H
+#include "lexem.h"
+void joinGotoAndLabel(Variable * lexemvar, vector <Lexem *> stack);
+void ifOperator(vector <Lexem *> &stackOfOperators, vector <Lexem *> &postfix_, vector <Lexem *> &infix, int i);
+vector <Lexem *> buildPostfix(vector <Lexem *> &infix);
+#endif
